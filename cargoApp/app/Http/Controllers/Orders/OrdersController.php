6d12d4c3717a771cea1b944248a0c3eb1e00d3c7
rawdasherif <rawdasherif119@gmail.com>
2019-06-14 15:55:40 +0200

    manage orders query

--- cargoApp/app/Http/Controllers/Orders/OrdersController.php
+++ cargoApp/app/Http/Controllers/Orders/OrdersController.php
@@ -20,12 +20,13 @@ class OrdersController extends Controller
 
     public function orders_list()
     {
-        $orders = Order::select('*')
-        ->leftjoin('company_order','orders.id','company_order.order_id')
+        $orders = Order::leftjoin('company_order','orders.id','company_order.order_id')
         ->leftjoin('driver_order','orders.id','driver_order.order_id')
         ->leftjoin('packages','orders.id','packages.order_id')
         ->leftjoin('companies','companies.id','company_order.sender_id')
-        ->leftjoin('drivers','drivers.id','driver_order.driver_id');
+        ->leftjoin('drivers','drivers.id','driver_order.driver_id')
+        ->select ('orders.*','packages.quantity','packages.Weight','packages.value','packages.pickup_location','packages.drop_off_location','packages.time_to_deliver','packages.height','packages.length','packages.width','companies.name as comp_name','drivers.name','drivers.phone')
+        ->orderBy('packages.created_at', 'desc');
 
      
         // ->orderBy('orders.created_at', 'desc');
