a3136d1391dbce7a4179de4f630b1f367bd0cf76
faten-ibrahim <faten_elssayad@yahoo.com>
2019-06-14 23:52:01 +0200

    use kawankoding/laravel-fcm

--- cargoApp/app/Http/Controllers/Api/Company/CompaniesOrdersController.php
+++ cargoApp/app/Http/Controllers/Api/Company/CompaniesOrdersController.php
@@ -52,42 +52,42 @@ class CompaniesOrdersController extends Controller
             'contact_name' => 'required',
             'contact_phone' => 'required',
             'address' => 'required',
-            'pickup_date'=>'required'
+            'pickup_date' => 'required'
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
 
-        $receiver_name=$request->receiver_name;
+        $receiver_name = $request->receiver_name;
         $receiver_company = Company::where('name', '=', $receiver_name)->first();
-        $id='';
+        $id = '';
         if ($receiver_company) {
             $company = CompanyContactList::where('receiver_name', '=', $request->receiver_name)->first();
-            $id=$receiver_company->id;
+            $id = $receiver_company->id;
             if (!$company) {
-                $CompanyContactList=CompanyContactList::create([
-                    'company_id'=>$request->sender_id,
+                $CompanyContactList = CompanyContactList::create([
+                    'company_id' => $request->sender_id,
                     'receiver_name' => $request->receiver_name,
                     'address_address' => $request->address,
-                    'contact_name'=>$request->contact_name,
-                    'contact_phone'=>$request->contact_phone,
+                    'contact_name' => $request->contact_name,
+                    'contact_phone' => $request->contact_phone,
                 ]);
             }
         } else {
-            $company_created=Company::create([
+            $company_created = Company::create([
                 'name' => $request->receiver_name,
                 'address' => $request->address,
                 'password' => Hash::make(str_random(8)),
-                'email' => str_random(5).'@gamil.com',
+                'email' => str_random(5) . '@gamil.com',
                 'phone' => $request->contact_phone,
-                'status'=>'contact',
+                'status' => 'contact',
             ]);
 
-            $company_created->status='contact';
+            $company_created->status = 'contact';
             $company_created->save();
 
-            $id=$company_created->id;
+            $id = $company_created->id;
         }
 
         // dd("hhhhhhhhhhhhhhhhhhherre");
@@ -95,9 +95,9 @@ class CompaniesOrdersController extends Controller
             'shipment_type' => $request->shipment_type,
             'car_number' => $request->car_number,
             'truck_type' => $request->truck_type,
-            'pickup_date'=> $request->pickup_date,
+            'pickup_date' => $request->pickup_date,
         ]);
-//  dd("hhhhhhhhhhhhhhhhhhhhhh");
+        //  dd("hhhhhhhhhhhhhhhhhhhhhh");
         $package = Package::create([
             'length' => $request->length,
             'width' => $request->width,
@@ -114,23 +114,36 @@ class CompaniesOrdersController extends Controller
             'order_id' => $order->id,
         ]);
 
-        $company_order=CompanyOrder::create([
-            'sender_id'=>$request->sender_id,
-            'receiver_id'=>$id,
-            'contact_name'=>$request->contact_name,
-            'order_id'=>$order->id,
+        $company_order = CompanyOrder::create([
+            'sender_id' => $request->sender_id,
+            'receiver_id' => $id,
+            'contact_name' => $request->contact_name,
+            'order_id' => $order->id,
         ]);
 
-        $orderNotify=array();
-        $orderNotify=array_merge($order->toArray(),$package->toArray());
-        $drivers_tokens=$this->driversNotification();
-        $notify =  new  DriverOrderNotification();
-        $notify->setDriverNotification($drivers_tokens,$orderNotify);
+        // Drivers Notifications
+        $orderNotify = array();
+        $orderNotify = array_merge($order->toArray(), $package->toArray());
+        $drivers_tokens = $this->driversNotification();
+        fcm()
+            ->to($drivers_tokens) // $recipients must an array
+            ->data([
+                'title' => 'Test driver FCM',
+                'body' => 'This is a test of driver FCM',
+            ])
+            ->send();
 
+        // Companies Notifications
+        $recipients = [];
+        $recipients = $this->get_tokens($request->sender_id, $id);
+        fcm()
+            ->to($recipients) // $recipients must an array
+            ->data([
+                'title' => 'Test company FCM',
+                'body' => 'This is a test of company FCM',
+            ])
+            ->send();
 
-        // dd($this->get_tokens ($request->sender_id,$id));
-        $notification=new CompanyOrderNotification();
-        $notification->setCompanyNotification($this->get_tokens ($request->sender_id,$id),$order);
         return response()->json([
             'message' => 'Order Saved Successfully',
             'order' => $order,
@@ -140,27 +153,26 @@ class CompaniesOrdersController extends Controller
         ], 201);
     }
 
-    public function get_tokens ($sender_id,$receiver_id)
+    public function get_tokens($sender_id, $receiver_id)
     {
-        $sender_token=CompanyToken::where('company_id', '=',$sender_id)->first()->token;
-        $receiver_token=CompanyToken::where('company_id', '=',$receiver_id)->first()->token;
+        $sender_token = CompanyToken::where('company_id', '=', $sender_id)->first()->token;
+        $receiver_token = CompanyToken::where('company_id', '=', $receiver_id)->first()->token;
 
-        $tokens =[];
-        array_push($tokens,$sender_token,$receiver_token);
+        $tokens = [];
+        array_push($tokens, $sender_token, $receiver_token);
 
         return $tokens;
     }
 
     public function driversNotification()
     {
-        $drivers_tokens=array();
-        $drivers=DriverToken::select('token')->get()->toArray();
-        for($i=0;$i<sizeof($drivers); $i++){
-            array_push($drivers_tokens,$drivers[$i]['token']);
+        $drivers_tokens = array();
+        $drivers = DriverToken::select('token')->get()->toArray();
+        for ($i = 0; $i < sizeof($drivers); $i++) {
+            array_push($drivers_tokens, $drivers[$i]['token']);
         }
 
 
         return $drivers_tokens;
     }
-
 }
