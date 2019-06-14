3dad467911315df8f295400d8fc74ee5eb2a7fba
rawdasherif <rawdasherif119@gmail.com>
2019-06-14 16:02:12 +0200

    orders index view

--- cargoApp/resources/views/orders/index.blade.php
+++ cargoApp/resources/views/orders/index.blade.php
@@ -2,9 +2,8 @@
 @section('content')
 <div class="container" style="width:100%;">
 @include('flash-message')
-<h2>Manage Supervisors</h2>
-<a class="btn btn-info" href="{{route('users.create')}}"><i class="fa fa-plus"></i><span>Add New Supervisor</span></a><br><br>
-<a class="btn btn-primary"href="{{route('supervisors.excel')}}"><i class="fa fa-download"></i><span>Export Supervisors</span></a><br><br>
+<h2>Manage Orders</h2>
+
 <table id="example" class="table table-striped" >
         <thead >
                 <th>Company name</th>
@@ -13,9 +12,8 @@
                 <th>Status</th>
                 <th>Estimated cost</th>
                 <th>Final cost</th>
-                <th>Final cost</th>
+                <th>Driver name</th>
                 <th>Driver phone</th>
-
             </tr>
         </thead>
     </table>
@@ -34,35 +32,15 @@
                 type: 'get',
             },
             columns: [
-                { data: 'name' },
+                { data: 'comp_name' },
                 { data: 'shipment_type' },
                 { data: 'pickup_date' },
                 { data: 'status' },
                 { data: 'estimated_cost' },
                 { data: 'final_cost' },
-                { data: 'final_cost' },
-                // { data: 'drivers_count' },
-                {
-                    mRender: function(data, type, row) {
-                         return '<span style="margin-left:5%;">'+row.drivers_count+'</>'+'<a href="/users/' + row.id + '" class="btn btn-link"><i class="fa fa-eye" data-toggle="tooltip" data-placement="top" title="Show"></i><span>show</span></a>'
-                            }
-                },
-                {
-                    mRender: function(data, type, row) {
-                         return '<a  style="margin-left:15px;" href="/users/' + row.id + '/edit" class="bttn btn btn-success btn-sm"><i class="fa fa-pencil" data-toggle="tooltip" data-placement="top" title="Edit"></i><span>Edit</span></a>'+
-                            '<form method="POST" action="users/'+row.id+'">@csrf {{ method_field('DELETE')}}<button  type="submit" onclick="return myFunction();" class="bttn btn btn-danger btn-sm"><i class="fa fa-trash" data-toggle="tooltip" data-placement="top" title="Delete"></i><span>Delete</span></button></form>'
-                            }
-                },
-                {
-                    mRender: function(data, type, row) {
-                            if (!row.banned_at)
-                                return '<a href="/users/' + row.id + '/ban" class=" btn btn-warning btn-sm" data-id="' + row.id + '" style="margin-left:10px;"><i class="fa fa-ban"></i><span>Deactive</span></a>'
-                            else
-                                return  '<a href="/users/' + row.id + '/unban" class="bttn btn btn-success" data-id="' + row.id + '" ><i class="fa fa-check"></i><span>Active</span></a>'
-
-                            }
-                },
-
+                { data: 'name' },
+                { data: 'phone' },
+  
             ],
 
             'lengthChange': true,
@@ -73,16 +51,6 @@
             'paging'      : true,
         } );
 
-                //confirm deleting
-                function myFunction(){
-                     var agree = confirm("Are you sure you want to delete this Supervisdor?");
-                        if(agree == true){
-                           return true
-                           }
-                           else{
-                           return false;
-                           }
-                     }
 
     </script>
 
