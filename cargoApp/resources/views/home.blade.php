a2c691af36c299a174aa540607873eec75f4976f
faten-ibrahim <faten_elssayad@yahoo.com>
2019-06-19 19:05:09 +0200

    edit supervisor dashboard view

--- cargoApp/resources/views/home.blade.php
+++ cargoApp/resources/views/home.blade.php
@@ -9,15 +9,18 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     <br>
-
+                    @role('admin')
                     <h5> Welcome to Admin Control Panal </h5>
-
+                    @endrole
+                    @role('supervisor')
+                    <h5> Welcome to Supervisor Control Panal </h5>
+                    @endrole
                     <br>
                 </div>
             </div>
